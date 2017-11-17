f28ebc4a00dd34bfe3342daa01baff91d5c3b596
Avery Thomas <athomas@bfgcom.com>
2017-11-17 17:38:43 -0500

    getting tweets by hashtag in php

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var app = angular.module('angularApp', ['ngResource']);
+
+app.controller('tweetsCtrl', ['$scope', '$resource', function ($scope, $resource) {
+
+    var consumeKey = 'ioMjOQKdQT7L7kISvoxgqJRvy';
+    var consumeSecret = 'rOxoxuJUuokN9V5PVQU23ZZMFnHAEsA0JcPDw6kiU2RvDpI3eR';
+
+}]);
